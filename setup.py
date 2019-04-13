066ed2318b8e15ee5322efd3d7e89e47d36a91c0
Henderson <henderson.hood@hendersonhood.com>
2019-04-12 23:44:25 -0400

    add back missin module cli

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="mycalc",
     version='0.1',
-    py_modules=['calc' ],
+    py_modules=['calc', 'cli' ],
     install_requires=[
         'Click' ,
         'pip>=19.0.3' ,

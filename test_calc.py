507140091f7dea35b3dd33edd8520228cf2b6bc6
Henderson <henderson.hood@hendersonhood.com>
2019-04-12 23:14:11 -0400

    update test_sub

--- test_calc.py
+++ test_calc.py
@@ -11,9 +11,9 @@ def test_add(n1, n2, sum):
 
 
 @pytest.mark.parametrize('n1, n2, result', [(5, 5, 0),
-                                             (-5, 5, -10),
-                                             (-10, -10, 0),
-                                             (0, 100, -100)
+                                             (15, 5, 10),
+                                             (100, -10, 90),
+                                             (100, 50, 50)
                                              ])
 def test_sub(n1, n2, result):
     assert result == calc.sub(n1, n1)

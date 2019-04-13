90404eda08a27779f89bbb687f9b818feed46bc8
Henderson <henderson.hood@hendersonhood.com>
2019-04-12 22:47:38 -0400

    add sub function

--- calc.py
+++ calc.py
@@ -1,3 +1,6 @@
 def add(num1, num2):
     """Will add two given numbers."""
     return int(num1 + num2)
+
+def sub(num1, num2):
+    pass
--- test_calc.py
+++ test_calc.py
@@ -9,3 +9,11 @@ import calc
 def test_add(n1, n2, sum):
     assert sum == calc.add(n1, n2) 
 
+
+@pytest.mark.parametrize('n1, n2, sum', [(5, 5, 0),
+                                             (-5, 5, 10),
+                                             (-10, -10, 0),
+                                             (0, 100, -100)
+                                             ])
+def test_sub(n1, n2, result):
+    assert result == calc.sub(n1, n1)

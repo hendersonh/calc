294d9f95aa412f9a711b531a7fe844c35be595e1
Henderson <henderson.hood@hendersonhood.com>
2019-04-13 21:39:32 -0400

    fix issues in sub function

--- cli.py
+++ cli.py
@@ -13,9 +13,9 @@ def add(num1, num2):
     print('{} + {} = {}'.format(num1, num2, result))
 
 @cli.command()
-@click.argument('num1')
-@click.argument('num2')
-def sub(num1, num2):
-    result = calc.sub(num1, num2)
-    print('{} - {} = {}'.format(num1, num2, result))
+@click.option('--n', '-n', type=(int, int))
+def sub(n):
+    """Enter <-n> follow by two number; we will subtract 2nd number from 1st number"""
+    result = calc.sub(n[0], n[1])
+    print('{} - {} = {}'.format(n[0], n[1], result))
      
